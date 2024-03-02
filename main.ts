351817780fe1f8ce727b1faba254e27bc975efed
QaimudinKhuwaja <159764940+QaimudinKhuwaja@users.noreply.github.com>
2024-03-02 12:36:44 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,81 @@
+//camelCase
+
+let myName:string = "Qaimudin Khuwaja"
+console.log(myName)
+
+//snake_case
+
+let fvrt_food:string = "Mutton Biryani"
+console.log(fvrt_food)
+
+//PascalCase
+
+let FvrtGame:string = "Pubg"
+console.log(FvrtGame)
+
+
+
+
+
+
+//Cheak Data Types
+
+//String
+
+let country = "Pakistan"
+console.log(typeof(country))
+
+//Number
+
+let myNum = 15
+console.log(typeof(myNum))
+
+//Boolean
+
+let password = "cdef456"
+console.log(password == "cdef456")
+console.log(password != "cdef456")
+console.log(password != "cdef4567")
+console.log(password == "cdeef456")
+console.log(typeof(password == "cdef456"))
+
+
+//any
+
+let num = 123
+let fname = "Faraz"
+let code = "123abc"
+console.log(typeof(num))
+console.log(typeof(fname))
+console.log(typeof(code == "123abc"))
+
+
+//Concatination
+let num1 = 5+8
+console.log(num1)
+
+let num2 = "5+8"
+console.log(num2)
+
+let num3 = 5+8
+console.log("num1")
+
+//Arthematics Operations
+
+let num4 = 72+5
+let num5 = 73+5
+console.log(num4 - num5)
+
+
+let numb = 6*2
+let numb1 = 12/6
+console.log(numb + numb1 * numb)
+
+let number = 8
+let number1 = 9
+console.log(number > number1)
+
+
+let number2 = 10
+let number3 = 14
+console.log(number2 < number3)
